d3fc3a3f71aa70e5d00008abf0a874ffea8a1b32
Vlad <vladismann@yandex.ru>
2023-07-25 20:23:23 +0300

    tests: draft

--- src/test/java/ru/practicum/shareit/ItemRequestServiceTest.java
+++ src/test/java/ru/practicum/shareit/ItemRequestServiceTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import ru.practicum.shareit.common.CustomPageRequest;
 import ru.practicum.shareit.item.dto.GetItemRequestItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repo.ItemRepo;
@@ -22,6 +25,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -31,13 +35,15 @@ public class ItemRequestServiceTest {
     private ItemRequestService itemRequestService;
 
     private final LocalDateTime created = LocalDateTime.now();
-    private final AddItemRequestDto addItemRequestDto = new AddItemRequestDto("TestD");
-    private final CreatedItemRequestDto createdItemRequestDto = new CreatedItemRequestDto((long) 1, "TestD", created);
-    private final ItemRequest itemRequest = new ItemRequest(1, "TestD", 1, created);
     private final User userOwner = new User(1, "Test", "Test@mail.ru");
+    private final ItemRequest itemRequest = new ItemRequest(1, "TestD", 1, created);
     private final Item item = new Item(1, "Test", "TestD", true, userOwner, (long) 1);
     private final GetItemRequestItemDto getItemRequestItemDto = new GetItemRequestItemDto(1, "Test", "TestD", true, 1);
+
+    private final AddItemRequestDto addItemRequestDto = new AddItemRequestDto("TestD");
+    private final CreatedItemRequestDto createdItemRequestDto = new CreatedItemRequestDto((long) 1, "TestD", created);
     private final GetItemRequestDto getItemRequestDto = new GetItemRequestDto(1, "TestD", created, List.of(getItemRequestItemDto));
+    private final Pageable pageable = new CustomPageRequest(1, 1, Sort.by("Test"));
 
     @Mock
     private ItemRepo itemRepo;
@@ -69,4 +75,16 @@ public class ItemRequestServiceTest {
         GetItemRequestDto actualItemRequest = itemRequestService.geById(1, 1);
         assertEquals(getItemRequestDto, actualItemRequest);
     }
+
+    @Test
+    public void geUserRequest() {
+        when(itemRequestRepo.existsById(any())).thenReturn(true);
+        when(itemRequestRepo.findByRequestorNot(anyLong(), any())).thenReturn(List.of(itemRequest));
+        when(itemRepo.findAllByRequestIdIn(List.of((long) 1))).thenReturn(List.of(item));
+
+        List<GetItemRequestDto> actualItemRequest = itemRequestService.getAllRequests(1, pageable);
+        assertEquals(List.of(getItemRequestDto), actualItemRequest);
+    }
+
+
 }
