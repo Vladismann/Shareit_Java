c038405ceec143eb696a0571a2f0f11880e63912
Vlad <vladismann@yandex.ru>
2023-07-25 19:11:09 +0300

    tests: draft

--- src/test/java/ru/practicum/shareit/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/BookingServiceTest.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static ru.practicum.shareit.booking.model.BookingStatus.APPROVED;
 import static ru.practicum.shareit.booking.model.BookingStatus.WAITING;
@@ -32,20 +33,20 @@ import static ru.practicum.shareit.booking.model.BookingStatus.WAITING;
 @SpringBootTest
 public class BookingServiceTest {
 
+    private BookingService bookingService;
+
     private final LocalDateTime startDate = LocalDateTime.now().plusHours(1);
     private final LocalDateTime endDate = LocalDateTime.now().plusHours(2);
-
-    private BookingService bookingService;
 
     private final User userBooker = new User(1, "Test", "Test@mail.ru");
     private final User userOwner = new User(2, "Test2", "Test2@mail.ru");
     private final Item item = new Item(1, "Test", "TestD", true, userOwner, null);
     private final BookingDto bookingDto = new BookingDto(1, LocalDateTime.now(), LocalDateTime.now().plusHours(1));
     private final Booking booking = new Booking(1, startDate, endDate, item, userOwner, WAITING);
-    Pageable pageable =  new CustomPageRequest(1, 1, Sort.by("Test"));
+    private final Pageable pageable = new CustomPageRequest(1, 1, Sort.by("Test"));
 
-    GetBookingUserDto getBookingUserDto = new GetBookingUserDto(2);
-    GetBookingItemDto getBookingItemDto = new GetBookingItemDto(1, "Test");
+    private final GetBookingUserDto getBookingUserDto = new GetBookingUserDto(2);
+    private final GetBookingItemDto getBookingItemDto = new GetBookingItemDto(1, "Test");
     private GetBookingDto getBookingDto;
 
 
@@ -100,6 +101,115 @@ public class BookingServiceTest {
         when(bookingRepo.findByBookerId(2, pageable)).thenReturn(List.of(booking));
 
         List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "ALL", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByBookerCurrent() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByBookerIdAndStartLessThanEqualAndEndGreaterThanEqual(anyLong(), any(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "CURRENT", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByBookerPast() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByBookerIdAndEndLessThan(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "PAST", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByBookerFuture() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByBookerIdAndStartGreaterThan(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "FUTURE", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByBookerWaiting() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findByBookerIdAndStatus(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "WAITING", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByBookerWaitingRejected() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findByBookerIdAndStatus(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByBooker(2, "REJECTED", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwner() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByOwner(2, pageable)).thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "ALL", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwnerCurrent() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllCurrentByOwner(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "CURRENT", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwnerPast() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllPastByOwner(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "PAST", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwnerFuture() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllFutureByOwner(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "FUTURE", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwnerWaiting() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByStatusByOwner(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "WAITING", pageable);
+        assertEquals(List.of(getBookingDto), createdBookingDto);
+    }
+
+    @Test
+    public void getBookingsByOwnerWaitingRejected() {
+        when(bookingRepo.existsById(any())).thenReturn(true);
+        when(bookingRepo.findAllByStatusByOwner(anyLong(), any(), any()))
+                .thenReturn(List.of(booking));
+
+        List<GetBookingDto> createdBookingDto = bookingService.getAllBookingsByOwner(2, "REJECTED", pageable);
         assertEquals(List.of(getBookingDto), createdBookingDto);
     }
 }
