fb7552b315e02818325116b29c03f862d556895d
Vladislav Sergeev <vladismann@yandex.ru>
2023-07-26 19:55:36 +0300

    tests: draft

--- src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,15 +20,15 @@ import ru.practicum.shareit.booking.service.BookingService;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.practicum.shareit.booking.BookingPaths.BOOKINGS_PATH;
+import static ru.practicum.shareit.booking.BookingPaths.OWNER_PATH;
 import static ru.practicum.shareit.common.CommonForControllers.BY_ID_PATH;
 import static ru.practicum.shareit.common.CommonForControllers.USER_HEADER;
 
@@ -70,6 +71,21 @@ public class BookingControllerTest {
     }
 
     @Test
+    public void approveBooking() throws Exception {
+        when(bookingService.approveBooking(anyLong(), anyLong(), anyBoolean())).thenReturn(getBookingDto);
+
+        MvcResult result = mvc.perform(patch(BOOKINGS_PATH + BY_ID_PATH, 1).content(mapper.writeValueAsString(bookingDto))
+                .header(USER_HEADER, 1)
+                .param("approved", "true")
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        GetBookingDto actualDto = mapper.readValue(result.getResponse().getContentAsString(), GetBookingDto.class);
+
+        assertEquals(getBookingDto, actualDto);
+    }
+
+    @Test
     public void getBooking() throws Exception {
         when(bookingService.getBooking(anyLong(), anyLong())).thenReturn(getBookingDto);
 
@@ -82,4 +98,35 @@ public class BookingControllerTest {
 
         assertEquals(getBookingDto, actualDto);
     }
+
+    @Test
+    public void getAllByBooker() throws Exception {
+        when(bookingService.getAllBookingsByBooker(anyLong(), any(), any())).thenReturn(List.of(getBookingDto));
+
+        MvcResult result = mvc.perform(get(BOOKINGS_PATH).content(mapper.writeValueAsString(bookingDto))
+                .header(USER_HEADER, 1)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        List<GetBookingDto> actualDto = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+
+        assertEquals(List.of(getBookingDto), actualDto);
+    }
+
+    @Test
+    public void getAllByOwner() throws Exception {
+        when(bookingService.getAllBookingsByOwner(anyLong(), any(), any())).thenReturn(List.of(getBookingDto));
+
+        MvcResult result = mvc.perform(get(BOOKINGS_PATH + OWNER_PATH).content(mapper.writeValueAsString(bookingDto))
+                .header(USER_HEADER, 1)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        List<GetBookingDto> actualDto = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+
+        assertEquals(List.of(getBookingDto), actualDto);
+    }
+
 }
