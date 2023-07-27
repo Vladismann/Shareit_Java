0db6dd0938fa82428a8af5886827028663694fa6
Vladislav Sergeev <vladismann@yandex.ru>
2023-07-27 12:26:56 +0300

    fix: date format

--- src/main/java/ru/practicum/shareit/common/Constants.java
+++ src/main/java/ru/practicum/shareit/common/Constants.java
@@ -2,5 +2,5 @@ package ru.practicum.shareit.common;
 
 public class Constants {
 
-    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 }
